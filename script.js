e0e642c3f539fc7e2f777bba6bc7ac94abc9568f
Mn-25b <76849040+Mn-25b@users.noreply.github.com>
2021-12-09 07:16:37 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ function money2string(n) {
   ['рубль', 'рубля', 'рублей'],//['доллар','доллара','долларов']
   ['тысяча', 'тысячи', 'тысяч'],
   ['миллион', 'миллиона', 'миллионов'],
-  ['миллиард', 'миллиарда', 'миллиардов']//,['трилион',...],[]
+  ['миллиард', 'миллиарда', 'миллиардов']
   ];
 
   function m999(n, b, f) {
@@ -67,78 +67,7 @@ function money2string(n) {
   
   return ucFirst(s);
 }
-// Определение наименования единицы денег: рубли, копейки
-// function plural(n, forms) {
-//   return forms[n % 10 == 1 && n % 100 != 11 ? 0 : n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2];
-// }
 
-// function money2string(number) {
-//   let stringNumber = [
-//     ['', '', ''],
-//     [
-//       ['один', 'один'],
-//       ['десять', 'одиннадцать', 'двенадцать', 'тринадцать', 'четырнадцать', 'пятнадцать', 'шестнадцать', 'семнадцать', 'восемнадцать', 'девятнадцать'], 'сто'
-//     ],
-//     [
-//       ['два', 'две'], 'двадцать', 'двести'
-//     ],
-//     ['три', 'тридцать', 'триста'],
-//     ['четыре', 'сорок', 'четыреста'],
-//     ['пять', 'пятьдесят', 'пятьсот'],
-//     ['шесть', 'шестьдесят', 'шестьсот'],
-//     ['семь', 'семьдесят', 'семьсот'],
-//     ['восемь', 'восемьдесят', 'восемьсот'],
-//     ['девять', 'девяносто', 'девятьсот']
-//   ];
-//   let unitMoney = [
-//     ['копейка', 'копейки', 'копеек'],
-//     ['рубль', 'рубля', 'рублей'], //['доллар','доллара','долларов'] или ['килограмм','килограмма','килограммов']
-//     ['тысяча', 'тысячи', 'тысяч'],
-//     ['миллион', 'миллиона', 'миллионов'],
-//     ['миллиард', 'миллиарда', 'миллиардов'] //,['трилион',...],[]
-//   ];
-
-//   function toStringNumber(number, unitMoney, currentDischarge) {
-//     let result = '';
-//     let t = stringNumber[Math.floor(number / 100) % 10][2];
-//     if (t) result += t + ' ';
-
-//     // Десятичная часть числа
-//     let decimalDischarge = Math.floor(number / 10) % 10; // number = 123, out: 2
-//     wordNumber = stringNumber[decimalDischarge][1]; // out: двадцать
-
-//     // Если у цифры есть склонения
-//     if (wordNumber instanceof Array) {
-//       wordNumber = wordNumber[number % 10];
-//       if (wordNumber) result += wordNumber + ' ';
-//     } else {
-//       if (wordNumber) result += wordNumber + ' ';
-//       wordNumber = stringNumber[number % 10][0]; // out: три
-//       if (wordNumber instanceof Array) wordNumber = wordNumber[penny == 0 || penny == 2 ? 1 : 0];
-//       if (wordNumber) result += wordNumber; // out: двадцать три
-//     }
-
-//     // Добавляем единицу измерения денег
-//     return result + ' ' + plural(number, unitMoney[currentDischarge]) + (currentDischarge > 1 ? ' ' : '');
-//   }
-
-//   // Округленное число     
-//   let roundedNumber = Math.floor(number),
-//     // Копейки
-//     penny = Math.floor(((number - roundedNumber) * 100) + 0.5),
-//     // Результат
-//     stringMoney = '';
-
-//   // Определение тысячной разрядности: тысяча, миллион, миллиард, ...
-//   for (let j = 1; roundedNumber > 0.9999; roundedNumber /= 1000) {
-//     console.log(j);
-//     stringMoney = toStringNumber(Math.floor(roundedNumber % 1000), unitMoney, j) + stringMoney;
-//     j++;
-//   }
-//   if (penny > 0) stringMoney = stringMoney + ' ' + toStringNumber(penny, unitMoney, 0)
-
-//   return ucFirst(stringMoney);
-// }
 
 // UpperCase для 1 буквы
 function ucFirst(str) {
